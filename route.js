79ce28fb764aeacbc73d25a7c3a8034fb12d0817
rohanchudasama20 <chudasamarohan808@gmail.com>
2025-07-29 19:26:58 +0530

    custom api user signup

---
+++ route.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const router=express.Router();
+
+const users=[];
+
+router.post('/signup',(req,res)=>{
+    const{email,password}=req.body;
+
+    const existinguser=users.find(user=>user.email===email);
+    if(existinguser){
+        return res.status(409).json({
+            messsage:"user is already exist"
+        });
+    }
+
+    const newuser={email,password};
+    users.push(newuser);
+
+    return res.status(201).json({
+        message:"sign up succesfull",
+        users:newuser
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app=express();
+const authroutes=require('./route');
+const port=3000;
+
+app.use(express.json());
+app.use('/api',authroutes);
+
+app.listen(port,()=>{
+    console.log(`server is running  on port ${port}`)
+});
+
